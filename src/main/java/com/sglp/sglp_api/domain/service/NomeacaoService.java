8ac695169362ae3a705dae29557fae5e50d13f42
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-06-14 18:48:41 -0300

    Refs. #111 - Refatoração de Nomeacao

--- src/main/java/com/sglp/sglp_api/domain/service/NomeacaoService.java
+++ src/main/java/com/sglp/sglp_api/domain/service/NomeacaoService.java
@@ -2,40 +2,42 @@ package com.sglp.sglp_api.domain.service;
 
 import com.sglp.sglp_api.domain.exception.NomeacaoNaoEncontradaException;
 import com.sglp.sglp_api.domain.model.Nomeacao;
-import com.sglp.sglp_api.domain.model.Processo;
 import com.sglp.sglp_api.domain.repository.NomeacaoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class NomeacaoService {
 
-    @Autowired
-    private NomeacaoRepository nomeacaoRepository;
-
-    @Autowired
-    private ProcessoService processoService;
+    private final NomeacaoRepository nomeacaoRepository;
 
     public List<Nomeacao> listar() {
         return nomeacaoRepository.findAll();
     }
 
-    public Nomeacao buscarOuFalhar(String nomeacaoId) {
+    public Nomeacao buscarPorIdOuFalhar(String nomeacaoId) {
         return nomeacaoRepository.findById(nomeacaoId)
                 .orElseThrow(() -> new NomeacaoNaoEncontradaException(nomeacaoId));
     }
 
     @Transactional
     public Nomeacao salvar(Nomeacao nomeacao) {
-        Processo processo = processoService.buscarPorIdOuFalhar(nomeacao.getProcesso().getId());
-
         return nomeacaoRepository.save(nomeacao);
     }
 
     public void remover(String nomeacaoId) {
         nomeacaoRepository.deleteById(nomeacaoId);
+    }
+
+    @Transactional
+    public Nomeacao atualizar(String nomeacaoId, Nomeacao nomeacao) {
+        Nomeacao nomeacaoExistente = buscarPorIdOuFalhar(nomeacaoId);
+        BeanUtils.copyProperties(nomeacao, nomeacaoExistente, "id");
+        return nomeacaoRepository.save(nomeacaoExistente);
     }
 }
