869f784a3698fe1b26c55083c7835bfdddc2e52f
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-08-28 12:09:39 -0300

    Refs. #212 - Ajustes na estratégia para que a IA tenha conhecimento do objetivo da perícia.

---
+++ src/main/java/com/sglp/sglp_api/domain/exception/QuebraDeRegraException.java
@@ -0,0 +1,8 @@
+package com.sglp.sglp_api.domain.exception;
+
+public class QuebraDeRegraException extends NegocioException {
+
+    public QuebraDeRegraException(String msg) {
+        super(msg);
+    }
+}
